6aeaab23977702580dfafef3d42587b13e6abdb7
Mauricio Ribeiro <mauricioribeiro1992@hotmail.com>
2018-05-21 10:00:32 -0300

    criacao da estrutura do back

---
+++ back/src/main/java/fatec/com/br/topicos/ServletInitializer.java
@@ -0,0 +1,13 @@
+package fatec.com.br.topicos;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TopicosApplication.class);
+	}
+
+}
---
+++ back/src/test/java/fatec/com/br/topicos/TopicosApplicationTests.java
@@ -0,0 +1,16 @@
+package fatec.com.br.topicos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopicosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
